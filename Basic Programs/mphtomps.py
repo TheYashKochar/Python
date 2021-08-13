9295c49b50c4ecf950545845cf4c892e49093c1a
Yash Kochar <yash.kochar143@gmail.com>
2021-08-13 15:50:01 +0530

    Added mphtomps

---
+++ Basic Programs/mphtomps.py
@@ -0,0 +1,6 @@
+# Converting Miles Per Hour to Meters Per Second
+print('Welcome to the MPH to MPS Converter!')
+#name = input('What is your name ?\n')
+#print('Hey! '+name+' This Program will convert MPH to MPS.')
+speed=input('Enter the Speed in MPH : ')
+print('The Speed in MPS is ' + str(round(float(speed)*0.44704,2)))
